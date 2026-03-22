c718be7ae38a1d0749980d787e223a3c6bf5edfb
pranavjayaprakash04 <pranavjayaprakash04@gmail.com>
2026-03-22 13:45:50 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,11 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   transpilePackages: ['three'],
-  output: 'standalone',
+  experimental: {
+    webpackBuildWorker: true,
+  },
   webpack: (config) => {
     config.externals = [...(config.externals || []), { canvas: 'canvas' }];
     return config;
   },
 };
-
 module.exports = nextConfig;
